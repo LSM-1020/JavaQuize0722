5a6d300e424ad62533a2eb1042fd19072e910b54
LSM-1020 <smmlee@naver.com>
2025-07-23 12:50:37 +0900

    답

--- src/Q_answer/Answer04.java
+++ src/Q_answer/Answer04.java
@@ -14,8 +14,10 @@ public class Answer04 {
 		
 		System.out.println("긴 영화인가요?" + sum.isLongMovie());
 		
-		
-		
+		Answer04Class sum2 = new Answer04Class(null, null, 0);
+		//기본 생성자가 아닌 지정한 생성자로 객체를 설정하면, 메모리에 객체가 새로 생성되면서
+		//생성자에서 지정한 값으로 초기화와 값이 바로 저장됨
+		//생성자를 만들지않고 만들면 기본값생성자가 들어감
 	}
 
 }
--- src/Q_answer/Answer04Class.java
+++ src/Q_answer/Answer04Class.java
@@ -6,6 +6,17 @@ public class Answer04Class {
 	String genre;
 	int runtime;
 	
+	public Answer04Class() { //기본 생성자 (생략됨)	
+	}
+	public Answer04Class(String title, String genre, int runtime) {// 새로운 생성자를 추가함
+		this.title = title;
+		this.genre = genre;
+		this.runtime = runtime;
+	}//this를 쓰면 본인 class안에있는 필드명을 지칭
+	 //메소드 이름이 같아도 메개변수가 다르면 다른메소드로 인식됨
+	
+	
+	
 	public boolean isLongMovie() {
 		return runtime >= 120;
 	}
